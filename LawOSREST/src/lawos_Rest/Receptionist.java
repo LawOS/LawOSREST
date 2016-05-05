dc14d8635125f1897f3e0625d29e4235b77f4d83
Kyriakos93 <Kyriakosb4a@yahoo.gr>
2016-05-05 17:48:34 +0300

    Added files via upload

--- LawOSREST/src/lawos_Rest/Receptionist.java
+++ LawOSREST/src/lawos_Rest/Receptionist.java
@@ -43,11 +43,11 @@ public class Receptionist {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Boolean response=false;
+		int response = 0;
 		java.sql.Statement stmt;
 		try {
 			stmt = conn.createStatement();
-			response = stmt.execute(
+			response = stmt.executeUpdate(
 					"INSERT INTO `appointment`(`Date`, `Time`, `Completed`, `clientID`, `legalStaffID`, `Case`) VALUES ('"
 							+ date + "', '" + time + "', 0, '" + clientID + "', " + legalStaffID + ", " + caseID
 							+ ");");
@@ -59,10 +59,56 @@ public class Receptionist {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if (response)
+		if (response == 1)
 			return "1";
 		else
 			return "0";
+	}
+
+	@Path("delapp")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String delAppointment(@FormParam("appointmentID") String appointmentID) {
+
+		Connection conn = null;
+		try {
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			// conn =
+			// DriverManager.getConnection("jdbc:mysql://localhost:3306/lawosdb?autoReconnect=true&useSSL=false&useUnicode=true&"+
+			// "user=root&password=root");
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://phpmyadmin.in.cs.ucy.ac.cy/cs363db?" + "user=cs363db&password=NjFU2pKz");
+
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Boolean response = false;
+		java.sql.Statement stmt;
+		try {
+			stmt = conn.createStatement();
+			response = stmt.execute("DELETE FROM `appointment` WHERE AppointmentID=" + appointmentID);
+			// rs.close();
+		} catch (SQLException e) {
+			System.err.println("[!]Problem with requested statement");
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "DELETE FROM `appointment` WHERE AppointmendID=" + appointmentID;
+		// if (response)
+		// return "1";
+		// else
+		// return "0";
 	}
 
 }
