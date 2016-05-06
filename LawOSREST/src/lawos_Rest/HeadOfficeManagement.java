6fd6216b91c98d45052c1bbaf0a41d413b2c947d
Kyriakos93 <Kyriakosb4a@yahoo.gr>
2016-05-06 14:43:01 +0300

    HeadOfficeManagement Completed
    Bug fixed in receptionist

--- LawOSREST/src/lawos_Rest/HeadOfficeManagement.java
+++ LawOSREST/src/lawos_Rest/HeadOfficeManagement.java
@@ -91,8 +91,7 @@ public class HeadOfficeManagement {
 		try {
 			stmt = conn.createStatement();
 			ResultSet rs = stmt
-					.executeQuery("SELECT DISTINCT Recommendation FROM `appointment` WHERE BranchID="
-							+ BranchID);
+					.executeQuery("SELECT DISTINCT Recommendation FROM `appointment` WHERE BranchID=" + BranchID);
 			response = GeneralServices.parseJSON(rs);
 			rs.close();
 		} catch (SQLException e) {
@@ -105,7 +104,7 @@ public class HeadOfficeManagement {
 
 		return response;
 	}// end of viewRecomPerBranch
-	
+
 	@Path("/reports/legalop/permonth")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
@@ -135,8 +134,7 @@ public class HeadOfficeManagement {
 		try {
 			stmt = conn.createStatement();
 			ResultSet rs = stmt
-					.executeQuery("SELECT DISTINCT LegalOpinion FROM `appointment` WHERE BranchID="
-							+ BranchID);
+					.executeQuery("SELECT DISTINCT LegalOpinion FROM `appointment` WHERE BranchID=" + BranchID);
 			response = GeneralServices.parseJSON(rs);
 			rs.close();
 		} catch (SQLException e) {
@@ -149,7 +147,7 @@ public class HeadOfficeManagement {
 
 		return response;
 	}// end of viewRecomPerBranch
-	
+
 	@Path("/reports/client/timeswait")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
@@ -179,8 +177,7 @@ public class HeadOfficeManagement {
 		try {
 			stmt = conn.createStatement();
 			ResultSet rs = stmt
-					.executeQuery("SELECT * FROM `appointment` WHERE (ClientID='"
-							+ ID + "' AND Completed=1)");
+					.executeQuery("SELECT * FROM `appointment` WHERE (ClientID='" + ID + "' AND Completed=1)");
 			response = GeneralServices.parseJSON(rs);
 			rs.close();
 		} catch (SQLException e) {
@@ -193,4 +190,136 @@ public class HeadOfficeManagement {
 
 		return response;
 	}// end of viewRecomPerBranch
+
+	@Path("/reports/weekly/perbranch/attended")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String getWeeklyClientsAttendedPerBranch(@FormParam("BranchID") String BranchID) {
+
+		Connection conn = null;
+		try {
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://phpmyadmin.in.cs.ucy.ac.cy/cs363db?" + "user=cs363db&password=NjFU2pKz");
+
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block.
+			e.printStackTrace();
+		}
+		String response = null;
+		java.sql.Statement stmt;
+
+		try {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(
+					"SELECT DAYOFWEEK(`Date`), COUNT(DISTINCT ClientID) FROM `appointment` WHERE (Completed=1 AND BranchID=" + BranchID
+							+ " AND Date <= CURDATE()) GROUP BY DAYOFWEEK(`Date`)");
+			response = GeneralServices.parseJSON(rs);
+			rs.close();
+		} catch (SQLException e) {
+			System.err.println("[!]Problem with requested statement");
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return response;
+	}// end of getWeeklyClientsAttendedPerBranch
+	
+	@Path("/reports/weekly/perbranch/all")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String getWeeklyClientsTotalVisitedPerBranch(@FormParam("BranchID") String BranchID) {
+
+		Connection conn = null;
+		try {
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://phpmyadmin.in.cs.ucy.ac.cy/cs363db?" + "user=cs363db&password=NjFU2pKz");
+
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block.
+			e.printStackTrace();
+		}
+		String response = null;
+		java.sql.Statement stmt;
+
+		try {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(
+					"SELECT DAYOFWEEK(`Date`), COUNT(DISTINCT ClientID) FROM `appointment` WHERE (BranchID=" + BranchID
+							+ " AND Date <= CURDATE()) GROUP BY DAYOFWEEK(`Date`)");
+			response = GeneralServices.parseJSON(rs);
+			rs.close();
+		} catch (SQLException e) {
+			System.err.println("[!]Problem with requested statement");
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return response;
+	}// end of getWeeklyClientsTotalVisitedPerBranch
+
+	@Path("/reports/weekly/perclient")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String getWeeklyPerClient(@FormParam("ClientID") String ClientID) {
+
+		Connection conn = null;
+		try {
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://phpmyadmin.in.cs.ucy.ac.cy/cs363db?" + "user=cs363db&password=NjFU2pKz");
+
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block.
+			e.printStackTrace();
+		}
+		String response = null;
+		java.sql.Statement stmt;
+
+		try {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(
+					"SELECT DAYOFWEEK(`Date`), COUNT(DISTINCT Recommendation), COUNT(DISTINCT LegalOpinion) FROM `appointment` WHERE (ClientID='" + ClientID
+							+ "' AND Date <= CURDATE()) GROUP BY DAYOFWEEK(`Date`)");
+			response = GeneralServices.parseJSON(rs);
+			rs.close();
+		} catch (SQLException e) {
+			System.err.println("[!]Problem with requested statement");
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return response;
+	}// end of getWeeklyPerClient
 }// end of class
